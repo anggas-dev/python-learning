f0aa7985cf2ee9602ed917e5914b7bbbd33b422a
Anggasnb <anggasaputraaintl@gmail.com>
2026-05-02 22:56:27 +0700

    menambahkan fitur supaya jika num2 =0 tidak terjadi error aja, tapi ada informasinya

--- mini-projects/calculator.py
+++ mini-projects/calculator.py
@@ -1,9 +1,17 @@
 # simple calculator
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+try:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-print("Addition:", num1 + num2)
-print("Subtraction:", num1 - num2)
-print("Multiplication:", num1 * num2)
-print("Division:", num1 / num2)
+    print("Addition:", num1 + num2)
+    print("Subtraction:", num1 - num2)
+    print("Multiplication:", num1 * num2)
+
+    if num2 != 0:
+    print("Division:", num1 / num2)
+    else :
+    print("DIvision: cannot divide by zero")
+
+except ValueError:
+    print("invalid input! Please enter number.")
